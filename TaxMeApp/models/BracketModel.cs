92c2ddb3d152934cb669d09cddefda99f73fddac
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-03-07 17:26:07 -0800

    set bounds from range to find median/mean

--- TaxMeApp/models/BracketModel.cs
+++ TaxMeApp/models/BracketModel.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace TaxMeApp.models
 {
@@ -12,6 +16,29 @@ namespace TaxMeApp.models
         public double PercentOfTaxableIncomePaid { get; set; }
         public double PercentOfGrossIncomePaid { get; set; }
         public double AverageTotalIncomeTax { get; set; }
+        public int LowerBound { get; private set; }
+        public int UpperBound { get; private set; }
+
+        public void SetBounds()
+        {
+            List<string> bounds = Regex.Matches(this.Range, @"(,*[\d]+,*[\d]*)+").Cast<Match>().Select(match => match.Value).ToList();
+            if (bounds.Count == 0)
+            {
+                this.LowerBound = 0;
+                this.UpperBound = 0;
+            }
+            else if (bounds.Count == 1)
+            {
+                this.LowerBound = int.Parse(bounds[0], NumberStyles.AllowThousands);
+                this.UpperBound = 15000000;
+            }
+            else
+            {
+                this.LowerBound = int.Parse(bounds[0], NumberStyles.AllowThousands);
+                this.UpperBound = int.Parse(bounds[1], NumberStyles.AllowThousands)-1;
+            }
+            Console.WriteLine("R: {0} | LB: {1} | UB: {2}", this.Range, this.LowerBound, this.UpperBound);
+        }
     }
 
 }
